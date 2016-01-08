fe023edc8d82cbeb4679fcafa1d3a7c4f3e84d49
karthik1106 <karthikkumar1106@gmail.com>
2016-01-08 18:01:13 +0530

    Sample Commit

---
+++ Sample/src/p1/Sample.java
@@ -0,0 +1,20 @@
+package p1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String path="F:\\QP_Latest\\testfiles\\largePic.jpg";
+		WebDriver driver=new FirefoxDriver();
+		driver.get("file:///F:/Sample/src/input.html");
+		driver.findElement(By.id("prodcert_upload")).sendKeys(path);
+		System.out.println("hello this is second sample git comment");
+System.out.println(System.getProperty("os.name"));
+System.out.println("hello this is second sample git");
+	}
+
+}
